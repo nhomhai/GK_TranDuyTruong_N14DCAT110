d8806a55285178978192abb97beb69f28575bbb8
duytruong2996 <37537726+duytruong2996@users.noreply.github.com>
2018-05-05 22:13:31 +0700

    TranDuyTruong 22:13 05/05/2018
    Giữa Kỳ - Android

---
+++ FireBaseGiuaKi/app/src/main/java/com/example/duytruong/firebasegiuaki/User.java
@@ -0,0 +1,35 @@
+package com.example.duytruong.firebasegiuaki;
+
+/**
+ * Created by duytr on 4/20/2018.
+ */
+
+public class User {
+    public String name;
+    public String email;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ FireBaseGiuaKi/app/src/main/java/com/example/duytruong/firebasegiuaki/UserAdapter.java
@@ -0,0 +1,73 @@
+package com.example.duytruong.firebasegiuaki;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by duytr on 4/20/2018.
+ */
+
+public class UserAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private List<User> arrUser;
+
+    public UserAdapter(Context context, int layout, List<User> arrUser) {
+        this.context = context;
+        this.layout = layout;
+        this.arrUser = arrUser;
+    }
+
+    @Override
+    public int getCount() {
+        return arrUser.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(layout, null);
+            viewHolder = new ViewHolder();
+
+            viewHolder.tvName = (TextView) view.findViewById(R.id.textviewName);
+            viewHolder.tvEmail = (TextView) view.findViewById(R.id.textviewEmail);
+            view.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) view.getTag();
+
+        }
+
+        User user = arrUser.get(position);
+
+        viewHolder.tvName.setText(user.getName());
+        viewHolder.tvEmail.setText(user.getEmail());
+
+
+        return view;
+    }
+
+    public class ViewHolder {
+        TextView tvName, tvEmail ;
+    }
+}
+
